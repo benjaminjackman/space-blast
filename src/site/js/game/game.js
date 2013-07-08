48d22df8c54e8dcf3479517f6dba09b5080e20ea
Ben Jackman <ben@jackman.biz>
2013-07-07 23:42:38 -0500

    viewport follows the ship
    Signed-off-by: Ben Jackman <ben@jackman.biz>

--- src/site/js/game/game.js
+++ src/site/js/game/game.js
@@ -1,18 +1,22 @@
 //Enable Console log in opera
-if(window.opera){ console = {log:window.opera.postError} }
+if (window.opera) { console = {log: window.opera.postError} }
 /**
  * This is the Main JS File
  */
 
-$(document).ready(function(){
-    //Init Crafty
-    Crafty.init(1280,1024);
-    //Add Canvas Element
-    Crafty.canvas.init();
-    //Set canvas under interface
-    Crafty.canvas._canvas.style.zIndex = '1';
-    
-    //play the loading scene
+$(document).ready(function () {
+  //Init Crafty
+  Crafty.init(2400, 2400);
+  //Add Canvas Element
+//  Crafty.canvas.init();
+  //Going to use a view port that is centered on the player
+  Crafty.viewport.init(1000, 1000);
+  //Set canvas under interface
+//  Crafty.canvas._canvas.style.zIndex = '1';
+  Crafty.viewport.clampToEntities = false;
+
+
+  //play the loading scene
     Crafty.scene("Level1");
 });
 
